5a63419042a36a6b725c25432d97b242490913a6
Manish Khadgi <manishkhadgi123@gmail.com>
2022-11-06 14:09:54 +1100

    modified

--- src/utilities/axiosHelper.js
+++ src/utilities/axiosHelper.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const fetchData = (str) => {
-  const apiEp = "http://www.omdbapi.com/?apikey=82d5b008&t=" + str;
+  const apiEp =
+    "http://www.omdbapi.com/?apikey=${process.env.REACT_API_API_KEY }&t=" + str;
   // console.log(str);
   const response = axios(apiEp);
   return response;
