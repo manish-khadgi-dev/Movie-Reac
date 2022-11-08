ff446e68263407062d8cef1ac9256fa6ef26be16
Manish Khadgi <manishkhadgi123@gmail.com>
2022-11-08 19:19:49 +1100

    donefinal

--- src/utilities/axiosHelper.js
+++ src/utilities/axiosHelper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const fetchData = (str) => {
-  const apiEp = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${str}`;
+  const apiEp = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&t=${str}`;
   // console.log(str);
   const response = axios(apiEp);
   return response;
